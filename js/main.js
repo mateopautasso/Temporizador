9455625f782eadd9a618dda40773431f1319ed06
mateopautassoPC <mateopautasso.mg@gmail.com>
2023-03-01 18:15:29 -0300

    Detalle

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ setInterval(()=>{
     if(segundosUser.innerHTML > '00') {
         segundosUser.style.filter = 'brightness(2)'
     } else segundosUser.style.filter = 'brightness(0.6)'
-},100)
+},50)
 
 const getTiempoFaltante = (fechaLimite) => {
     let tiempoActual = new Date();
